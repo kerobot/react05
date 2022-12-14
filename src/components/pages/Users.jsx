053ec398abeced2af7f7c883e746de93d2d54935
Manabu Kikuchi <16217397+kerobot@users.noreply.github.com>
2022-12-14 15:06:50 +0900

    Initial commit

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import { useRecoilState } from "recoil";
+
+// import { UserContext } from "../../providers/UserProvider";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+import { userState } from "../../store/userState";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `太郎-${val}`,
+    image: "https://source.unsplash.com/eoqnr8ikwFE",
+    email: "12345@example.com",
+    phone: "999-9999-9999",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "http://google.com"
+  };
+});
+
+export const Users = () => {
+  // const { userInfo, setUserInfo } = useContext(UserContext);
+  const [userInfo, setUserInfo] = useRecoilState(userState);
+  const isAdmin = userInfo ? userInfo.isAdmin : false;
+
+  const onClickSwitch = () => setUserInfo({ isAdmin: !isAdmin });
+
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>切り替え</SecondaryButton>
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
---
+++ src/router/Router.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import { Top } from "../components/pages/Top";
+import { Users } from "../components/pages/Users";
+import { DefaultLayout } from "../components/templete/DefaultLayout";
+import { HeaderOnly } from "../components/templete/HeaderOnly";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <DefaultLayout>
+            <Top />
+          </DefaultLayout>
+        </Route>
+        <Route path="/users">
+          <HeaderOnly>
+            <Users />
+          </HeaderOnly>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
